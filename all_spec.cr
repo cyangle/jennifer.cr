52210ff7bf37adb401ee215f2d86a73868862b46
Chao Yang <cyang05@outlook.com>
2022-07-24 21:02:27 +0000

    Update working specs

--- all_spec.cr
+++ all_spec.cr
@@ -95,6 +95,9 @@ require "./spec/relation/polymorphic_has_many_spec.cr"
 require "./spec/relation/polymorphic_has_one_spec.cr"
 require "./spec/relation/polymorphic_belongs_to_spec.cr"
 require "./spec/model/enum_converter_spec.cr"
+require "./spec/adapter/base_sql_generator_spec.cr"
+require "./spec/query_builder/query_spec.cr"
+require "./spec/model/relation_definition_spec.cr"
 
 
 # Below spec is too slow to run
--- bad_spec.cr
+++ bad_spec.cr
@@ -1,5 +1,2 @@
 # https://github.com/crystal-lang/crystal/issues/12296
-require "./spec/adapter/base_sql_generator_spec.cr"
-require "./spec/query_builder/query_spec.cr"
-require "./spec/model/relation_definition_spec.cr"
 require "./spec/query_builder/ordering_spec.cr"
